ce7d39a93e838f6b17ed7989be0a90c3937abb9b
YOUCEF TALIBI <117942963+youce-etalibi@users.noreply.github.com>
2025-10-24 16:51:43 +0100

    Change app port from 5000 to 5001

--- app.py
+++ app.py
@@ -262,4 +262,4 @@ def check_ip():
 
 if __name__ == '__main__':
     from os import environ
-    app.run(host="0.0.0.0", port=int(environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(environ.get("PORT", 5001)))
